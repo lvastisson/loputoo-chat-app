640640cfb54af0c91b1e276bcf462266136f1efa
lvastisson <lauri.viljan.vastisson@gmail.com>
2023-03-08 21:45:33 +0200

    socket.io algeline ühendus olemas serveri ja kliendi vahel, installis teise C# socket.io libra

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,32 @@
 import express, { Application } from "express";
 import { connectToDatabase } from "./services/database.service";
 import { messagesRouter } from "./routes/messages.router";
+import { Server } from "socket.io";
+// import { createServer } from "http";
+// import {
+//   ClientToServerEvents,
+//   ServerToClientEvents,
+//   InterServerEvents,
+//   SocketData,
+// } from "./interfaces/socketio.interfaces";
 
 const app: Application = express();
+
+// const server = createServer(app);
+// const io = new Server(server);
+
+const io = new Server(8888);
+
+io.on("connection", (socket) => {
+  console.log("user connected");
+
+  socket.emit("hello", "ühendus olemas");
+
+  socket.on("disconnect", function () {
+    console.log("user disconnected");
+  });
+});
+
 const port = 5000;
 
 connectToDatabase()
@@ -11,6 +35,16 @@ connectToDatabase()
 
     app.listen(port, () => {
       console.log(`Server started at http://localhost:${port}`);
+
+      // io.on("connection", (socket) => {
+      //   console.log("user connected");
+
+      //   socket.emit("hello", "ühendus olemas");
+
+      //   socket.on("disconnect", function () {
+      //     console.log("user disconnected");
+      //   });
+      // });
     });
   })
   .catch((error: Error) => {
--- frontend-app/frontend-app/Form1.cs
+++ frontend-app/frontend-app/Form1.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using SocketIOClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +14,7 @@ namespace frontend_app
 {
     public partial class Form1 : Form
     {
+        public delegate void UpdateLabelMethod(string text, bool isLabel1);
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +23,51 @@ namespace frontend_app
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine("clicked");
+            socketIoManager();
+        }
+
+        private async void socketIoManager()
+        {
+            var client = new SocketIO("http://localhost:8888/");
+            Debug.WriteLine("past socket initalization");
+
+            client.OnConnected += async (sender, e) =>
+            {
+                Debug.WriteLine("ühendatud");
+                UpdateStatus("ühendatud", false);
+            };
+
+            client.On("hello", (data) =>
+            {
+                Debug.WriteLine(data.GetValue<string>());
+                UpdateStatus(data.GetValue<string>(), true);
+            });
+
+            await client.ConnectAsync();
+        }
+
+        private void UpdateStatus(string text, bool isLabel1)
+        {
+            if (isLabel1 ? this.label1.InvokeRequired : this.label2.InvokeRequired)
+            {
+                UpdateLabelMethod del = new UpdateLabelMethod(UpdateStatus);
+                this.Invoke(del, new object[] { text, isLabel1 });
+            }
+            else
+            {
+                if (isLabel1)
+                {
+                    this.label1.Text = text;
+                } else
+                {
+                    this.label2.Text = text;
+                }
+            }
         }
     }
 }
