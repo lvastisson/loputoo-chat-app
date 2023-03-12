772e9ae61fc55cc3257c3344c606775485289994
lvastisson <lauri.viljan.vastisson@gmail.com>
2023-03-12 21:40:27 +0200

    lisas testimiseks /login api, reaalne sessioon pole veel implementeeritud

--- backend/src/routes/users.router.ts
+++ backend/src/routes/users.router.ts
@@ -2,8 +2,9 @@ import express, { Request, Response } from "express";
 import { ObjectId } from "mongodb";
 import { collections } from "../services/database.service";
 import User from "../models/user";
-import { RegisterDto } from "../interfaces/useractions.interfaces";
+import { LoginDto, RegisterDto } from "../interfaces/useractions.interfaces";
 import * as bcrypt from "bcrypt";
+import * as crypto from "crypto";
 
 export const usersRouter = express.Router();
 
@@ -27,6 +28,32 @@ usersRouter.post("/register", async (req: Request, res: Response) => {
     result
       ? res.status(201).send(`Successfully registered new user with id ${result.insertedId}`)
       : res.status(500).send("Failed to register user.");
+  } catch (error: any) {
+    console.error(error);
+    res.status(400).send(error.message);
+  }
+});
+
+usersRouter.post("/login", async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body as LoginDto;
+
+    const user = (await collections.users?.findOne<User>({ email: email })) as User;
+    if (!user) return res.status(400).send(`User with email ${email} doesn't exist`);
+
+    const passwordMatches = await bcrypt.compare(password, user.passwordHash);
+    if (!passwordMatches) return res.status(400).send("Wrong password");
+
+    const generatedRandomID = crypto.randomBytes(8).toString("hex");
+
+    const result = await collections.users?.updateOne(
+      { _id: new ObjectId(user.id) },
+      { $set: { sessionId: generatedRandomID } }
+    );
+
+    result
+      ? res.status(201).send(`Successfully generated session for user ${user.username} with id ${generatedRandomID}`)
+      : res.status(500).send("Failed to generate session");
   } catch (error: any) {
     console.error(error);
     res.status(400).send(error.message);
