80c55ef66e41723c72b77d10aa7ac36dde9ea814
lvastisson <lauri.viljan.vastisson@gmail.com>
2023-04-06 12:59:04 +0300

    change api response to object

--- backend/src/routes/users.router.ts
+++ backend/src/routes/users.router.ts
@@ -16,10 +16,10 @@ usersRouter.post("/register", async (req: Request, res: Response) => {
     const { email, username, password } = req.body as RegisterDto;
 
     const checkUsernameExists = (await collections.users?.findOne<User>({ username: username })) as User;
-    if (checkUsernameExists) return res.status(400).send(`User with username ${username} already exists`);
+    if (checkUsernameExists) return res.status(400).send({ message: `User with username ${username} already exists` });
 
     const checkEmailExists = (await collections.users?.findOne<User>({ email: email })) as User;
-    if (checkEmailExists) return res.status(400).send(`User with email ${email} already exists`);
+    if (checkEmailExists) return res.status(400).send({ message: `User with email ${email} already exists` });
 
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = { email, username, passwordHash: hashedPassword } as User;
@@ -27,11 +27,11 @@ usersRouter.post("/register", async (req: Request, res: Response) => {
     const result = await collections.users?.insertOne(newUser);
 
     result
-      ? res.status(201).send(`Successfully registered new user with id ${result.insertedId}`)
-      : res.status(500).send("Failed to register user.");
+      ? res.status(201).send({ message: `Successfully registered new user with id ${result.insertedId}` })
+      : res.status(500).send({ message: "Failed to register user." });
   } catch (error: any) {
     console.error(error);
-    res.status(400).send(error.message);
+    res.status(400).send({ message: error.message });
   }
 });
 
@@ -53,18 +53,18 @@ usersRouter.post("/login", async (req: Request, res: Response) => {
     );
 
     result
-      ? res.status(201).send(`Successfully generated session for user ${user.username} with id ${generatedRandomID}`)
-      : res.status(500).send("Failed to generate session");
+      ? res.status(201).send({ message: `Successfully generated session for user ${user.username} with id ${generatedRandomID}`, token: generatedRandomID })
+      : res.status(500).send({ message: "Failed to generate session" });
   } catch (error: any) {
     console.error(error);
-    res.status(400).send(error.message);
+    res.status(400).send({ message: error.message });
   }
 });
 
 usersRouter.post("/logout", isAuth, async (req: Request, res: Response) => {
   try {
     const user = (await collections.users?.findOne<User>({ sessionId: req.session?.sessionid })) as User;
-    if (!user) return res.status(400).send(`Session doesn't exist`);
+    if (!user) return res.status(400).send({ message: "Session doesn't exist" });
 
     const result = await collections.users?.updateOne(
       { _id: new ObjectId(user.id) },
@@ -72,10 +72,10 @@ usersRouter.post("/logout", isAuth, async (req: Request, res: Response) => {
     );
 
     result
-      ? res.status(201).send(`Successfully logged out user ${user.username} with id ${user.sessionId}`)
-      : res.status(500).send("Failed to logout");
+      ? res.status(201).send({ message: `Successfully logged out user ${user.username} with id ${user.sessionId}` })
+      : res.status(500).send({ message: 'Failed to logout' });
   } catch (error: any) {
     console.error(error);
-    res.status(400).send(error.message);
+    res.status(400).send({ message: error.message });
   }
 });
